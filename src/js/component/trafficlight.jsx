183e42335a8d57845b41f9eb3bd3cc08d5478f1c
LeserraJ <leserrabusiness@gmail.com>
2023-01-26 01:14:11 +0000

    fixed lights being replaced by glow

--- src/js/component/trafficlight.jsx
+++ src/js/component/trafficlight.jsx
@@ -10,9 +10,9 @@ const TrafficLight = () =>{
 return(
 <>      
 <div className = "container">
-      <div className ={"red" + (color === "red" ? "red + glow" : "")} onClick={() => setColor("red")} ></div>
-      <div className ={"yellow" + (color=== "yellow" ? "yellow + glow" : "")} onClick={() => setColor("yellow")} ></div>
-      <div className ={"green" + (color === "green" ? "green + glow" : "")} onClick={() => setColor("green")} ></div>
+      <div className ={(color === "red" ? "red + glow" : "red")} onClick={() => setColor("red")} ></div>
+      <div className ={(color=== "yellow" ? "yellow + glow" : "yellow")} onClick={() => setColor("yellow")} ></div>
+      <div className ={(color === "green" ? "green + glow" : "green")} onClick={() => setColor("green")} ></div>
 </div>
 </>
 //If you used to conditionally omit it with onClick={condition && value}, pass onClick={condition ? value : undefined} instead. 
